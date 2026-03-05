f845847394832003a7609f4e5dc2659b14459854
yewon0325 <yewon3022@naver.com>
2026-03-05 15:32:53 +0900

    수정

--- chapter01_OpenCV/01_load_and_grayscale.py
+++ chapter01_OpenCV/01_load_and_grayscale.py
@@ -1,15 +1,13 @@
 import cv2 as cv
 import numpy as np
 import sys
-import argparse
 import os
 
 def main():
-    img_path = "soccer.jpg"  
+    img_path = "soccer.jpg"
     if not os.path.exists(img_path):
         print("파일이 존재하지 않습니다.")
         sys.exit(1)
-
     # 1) 이미지 로드
     img = cv.imread(img_path)
     if img is None:
@@ -25,8 +23,11 @@ def main():
     # 4) 가로로 연결
     combined = np.hstack((img, gray_bgr))
 
-    # 5) 출력 + 아무 키나 누르면 닫기
-    cv.imshow("Original | Grayscale", combined)
+    # 5)캡처처럼 fx, fy로 축소 (예: 0.5 = 반으로)
+    combined_small = cv.resize(combined, dsize=(0, 0), fx=0.5, fy=0.5)
+
+    # 6) 출력 + 아무 키나 누르면 닫기
+    cv.imshow("Original | Grayscale (small)", combined_small)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
